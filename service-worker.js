4204f7ac2aa7a087f414fa39d543b87761dee933
jlalanda03 <Javier.lalanda@gmail.com>
2026-06-22 15:52:29 +0200

    Bump cache after anti-repeat update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='bde-test-lab-v1';
+const CACHE='bde-test-lab-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
