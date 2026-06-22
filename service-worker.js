503a47b3a69084eda08837e62a29c8da6b49b842
jlalanda03 <Javier.lalanda@gmail.com>
2026-06-22 16:14:06 +0200

    Cache expanded question bank assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='bde-test-lab-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='bde-test-lab-v5';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./data/q1.js','./data/q2.js','./data/q3.js','./data/q4.js','./data/questions.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
